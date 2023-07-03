4a356355aa821b587a39e309743813ef25ddeba0
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-07-03 09:20:40 +0200

    Delete fix

--- Recipes/index.js
+++ Recipes/index.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.engine('hbs', exphbs.engine({
     extname: '.hbs',
@@ -13,8 +13,9 @@ app.engine('hbs', exphbs.engine({
 
 app.set('view engine', 'hbs')
 app.get('/', async (req, res) => {
-  res.render('start')
+    res.render('start')
 })
+
 app.get('/recipes', async (req, res) => {
     const client = new mongodb.MongoClient("mongodb://127.0.0.1")
     await client.connect()
@@ -31,38 +32,35 @@ app.get('/recipes/new', async (req, res) => {
     res.render('new')
 })
 
-app.post('/recipes/new', async (req, res) =>{
+app.post('/recipes/new', async (req, res) => {
     const recipe = {
         Title: req.body.title,
         Summary: req.body.summary,
         Minutes: req.body.minutes
     }
-    
+
     const client = new mongodb.MongoClient("mongodb://127.0.0.1")
-     client.connect()
+    client.connect()
     const db = client.db("MyRecipeDB")
 
-  await db.collection("Recipes").insertOne(recipe);
-  res.redirect('/recipes')
+    await db.collection("Recipes").insertOne(recipe);
+    res.redirect('/recipes')
 })
 
-app.get('/recipes/:id', async (req, res) => {
-    res.render('delete')
+app.get('/recipes/delete/:id', async (req, res) => {
+    res.render('delete', { id: req.params.id })
 })
 
-app.delete('/recipes/:id', async (req, res) => {
-    console.log("hello");
+app.post('/recipes/delete/:id', async (req, res) => {
     const _id = new mongodb.ObjectId(req.params.id)
     const client = new mongodb.MongoClient("mongodb://127.0.0.1:27017")
     await client.connect()
-   
+
     const db = client.db("MyRecipeDB")
-  
-  db.collection("Recipes").deleteOne({_id: _id}, () => {
+
+    await db.collection("Recipes").deleteOne({ _id: _id })
+
     res.redirect("/recipes")
-   })
-
-
 })
 
 
